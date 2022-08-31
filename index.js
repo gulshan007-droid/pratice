30d90c14c8bd02a4d94a34a8d74172d3dd3626bf
gulshan007-droid <gulshanverma716@gmail.com>
2022-08-31 16:32:26 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-var name = 'gulshan';
-console.log(name);
+var students = [
+  { name: 'raja', age: 39, id: 1 },
+  { name: 'gurumeet', age: 49, id: 2 },
+  { name: 'abhishek', age: 69, id: 3 },
+  { name: 'viru', age: 49, id: 2 },
+  { name: 'vivek', age: 69, id: 3 },
+];
+const filteredOutput = students.filter((student) => student.age > 39);
+filteredOutput.map((item) => console.log(item.name));
